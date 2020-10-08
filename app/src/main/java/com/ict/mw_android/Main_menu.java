cdac032310b77a07fe7d25bf86109c382b135c5d
wjdghkscks <hwamchanny@naver.com>
2020-10-08 18:09:58 +0900

    CREATE Ask

---
+++ app/src/main/java/com/ict/mw_android/Main_menu.java
@@ -0,0 +1,14 @@
+package com.ict.mw_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
