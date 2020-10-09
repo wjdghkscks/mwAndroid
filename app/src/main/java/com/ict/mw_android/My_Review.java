3dd4626829f3e9bea42656798e9374a383f987f3
icebread <hwamchanny@naver.com>
2020-10-09 18:01:42 +0900

    SET design

---
+++ app/src/main/java/com/ict/mw_android/My_Review.java
@@ -0,0 +1,14 @@
+package com.ict.mw_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class My_Review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my__review);
+    }
+}
